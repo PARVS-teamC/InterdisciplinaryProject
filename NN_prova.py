dacd5c00da28e836db5550ec97491e89be3ddedf
Miriana <121249605+MirianaPassarotto@users.noreply.github.com>
2024-03-07 10:07:35 +0100

    Threshold intro
    substituted cdma error + PELT with threshold comparison

--- NN_prova.py
+++ NN_prova.py
@@ -1,18 +1,13 @@
-import pandas as pd
-from scipy.stats import zscore
-from keras.layers import Input, Conv1D, Conv1DTranspose, AveragePooling1D, Activation,Dense,Reshape,Flatten
-from keras.models import Model
-from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
-import tensorflow as tf
-from keras.activations import tanh, linear
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+import keras
+from keras import layers
+from matplotlib import pyplot as plt
 
 def moving_zscore(df, window_size):
     df_norm = df.copy()
-    for col in df_norm.columns[1:]:
-        df_norm[col] = (df_norm[col] - df_norm[col].rolling(window=window_size,min_periods=1).mean()) / (df_norm[col].rolling(window=window_size,min_periods=1).std()+0.0001)
+    for col in df_norm.columns:
+        df_norm[col] = (df_norm[col] - df_norm[col].rolling(window=window_size,min_periods=1).mean()) / (df_norm[col].rolling(window=window_size,min_periods=1).std())
     df_norm = df_norm.dropna() 
     return df_norm
 
@@ -24,100 +19,148 @@ def segmentation(df, window_size):
         segments.append(segment)
     return segments
 
-def compute_cumulative_dma_error(reconstructed_data, sensor_indices,test_data):
-    ns = len(sensor_indices)
-    num_segments = reconstructed_data.shape[0]
-    num_timestamps = reconstructed_data.shape[1]
-    
-    dma_errors = np.zeros(num_timestamps)
+window_size = 8640
+window_seg = 288
+df_dataset = pd.read_csv('Data/2019_SCADA_Pressures.csv', sep=';')
+for column in df_dataset.columns[1:]:
+    df_dataset[column] = pd.to_numeric(df_dataset[column].str.replace(',', '.'))
+df_train = df_dataset.iloc[:4033, :4]
+df_test = df_dataset.iloc[4034:52000, :4]
+#Timeseries data (train) without anomalies
+fig, ax = plt.subplots()
+df_train.plot(legend=False, ax=ax)
+plt.show()
+#Timeseries data (test) with anomalies
+fig, ax = plt.subplots()
+df_test.plot(legend=False, ax=ax)
+plt.show()
 
-    for j in range(num_timestamps):
-        sre_per_timestamp = []
-        for seg_idx in range(num_segments):
-            reconstructed_data_j = reconstructed_data[seg_idx, j, :]
-            sre = np.zeros(ns)
-            for idx, sensor_idx in enumerate(sensor_indices):
-                sensor_data = reconstructed_data_j[sensor_idx]
-                sre[idx] = np.mean(np.abs(sensor_data - test_data[seg_idx, j, sensor_idx]))
-            sre_per_timestamp.append(np.sum(sre))
-        
-        # Compute median reconstruction error for current timestamp
-        median_sre = np.median(sre_per_timestamp)
-        dma_errors[j] = median_sre
-    
-    return dma_errors
+df_train=df_train.drop('Timestamp', axis=1)
+
+df_train_value=moving_zscore(df_train,window_size)
+print("Number of training samples:", len(df_train_value))
+fig, ax = plt.subplots()
+df_train_value.plot(legend=False, ax=ax)
+plt.show()
+
+x_train=segmentation(df_train_value, window_seg)
+x_train=np.array(x_train)
 
 
-def oneD_CNN(data,test_data):
+model = keras.Sequential(
+    [
+        layers.Input(shape=(x_train.shape[1], x_train.shape[2])),
+        layers.Conv1D(
+            filters=32,
+            kernel_size=7,
+            padding="same",
+            strides=1,
+            activation="tanh",
+        ),
+        layers.AveragePooling1D(pool_size=2),
+        layers.Conv1D(
+            filters=16,
+            kernel_size=7,
+            padding="same",
+            strides=1,
+            activation="tanh",
+        ),
+        layers.AveragePooling1D(pool_size=2),
+        layers.Conv1DTranspose(
+            filters=16,
+            kernel_size=7,
+            padding="same",
+            strides=4,
+            activation="tanh",
+        ),
+        layers.AveragePooling1D(pool_size=2),
+        layers.Conv1DTranspose(
+            filters=32,
+            kernel_size=7,
+            padding="same",
+            strides=4,
+            activation="tanh",
+        ),
+        layers.AveragePooling1D(pool_size=2),
+        #last layer
+        layers.Flatten(),
+        layers.Dense(288 * 3, activation='linear'),
+        layers.Reshape((288, 3))
+    ]
+)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
+model.summary()
+history = model.fit(
+    x_train,
+    x_train,
+    epochs=25,
+    batch_size=144,
+    validation_split=0.2,
+    callbacks=[
+        keras.callbacks.EarlyStopping(monitor="val_loss")
+    ],
+)
+plt.plot(history.history["loss"], label="Training Loss")
+plt.plot(history.history["val_loss"], label="Validation Loss")
+plt.legend()
+plt.show()
 
-    encoder = tf.keras.models.Sequential([
-        Conv1D(filters=32, kernel_size=7, strides=1, activation='tanh'),
-        AveragePooling1D(pool_size=2),
-        Conv1D(filters=16, kernel_size=7, strides=1, activation='tanh'),
-        AveragePooling1D(pool_size=2)
-    ])
+x_train_pred = model.predict(x_train)
+train_mae_loss = np.mean(np.abs(x_train_pred - x_train), axis=1)
 
-    # Decoder
-    decoder = tf.keras.models.Sequential([
-        Conv1DTranspose(filters=16, kernel_size=7, strides=4, activation='tanh'),
-        AveragePooling1D(pool_size=2),
-        Conv1DTranspose(filters=32, kernel_size=7, strides=4, activation='tanh'),
-        AveragePooling1D(pool_size=2),
-        Flatten(),
-        Dense(288 * 33, activation='linear'),
-        Reshape((288, 33))
-    ])
+plt.hist(train_mae_loss, bins=50)
+plt.xlabel("Train MAE loss")
+plt.ylabel("No of samples")
+plt.show()
 
-    autoencoder = tf.keras.models.Sequential([
-        encoder,
-        decoder
-    ])
+threshold = np.max(train_mae_loss)
+print("Reconstruction error threshold: ", threshold)
 
-    autoencoder.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='mse', metrics=['mse', 'mae'])
 
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', restore_best_weights=True)
+plt.plot(x_train[0])
+plt.plot(x_train_pred[0])
+plt.show()
 
-    autoencoder.fit(data, data, batch_size=144, epochs=14, validation_split=0.2, callbacks=[early_stopping])
+# plt.plot(x_train[0,:,0])
+# plt.plot(x_train_pred[0,:,0])
+# plt.show()
+# plt.plot(x_train[0,:,1])
+# plt.plot(x_train_pred[0,:,1])
+# plt.show()
+# plt.plot(x_train[0,:,2])
+# plt.plot(x_train_pred[0,:,2])
+# plt.show()
 
-    autoencoder.summary()
-    #evaluation=autoencoder.evaluate(test_data, test_data)
-    #print("Evaluation Loss:", evaluation)
-    reconstructed_data = autoencoder.predict(test_data)
-    
-    return reconstructed_data
+df_test=df_test.drop('Timestamp', axis=1)
+df_test_value = moving_zscore(df_test,window_size)
+fig, ax = plt.subplots()
+df_test_value.plot(legend=False, ax=ax)
+plt.show()
 
-if __name__ == '__main__':
-    window_size = 8640
-    window_seg = 288
-    df = pd.read_csv('Data/2019_SCADA_Pressures.csv', sep=';')#('/content/2019_SCADA_Pressures.csv', sep=';')
-    df_train = df[:4033]
-    df_test= df[4034:50000]
-    for col in df_train.columns[1:]:
-        df_train.loc[:, col] = pd.to_numeric(df_train[col].str.replace(',', '.')).astype('float32')
-        df_test.loc[:, col] = pd.to_numeric(df_test[col].str.replace(',', '.')).astype('float32')
-    #train moving score    
-    df_norm_train = moving_zscore(df_train, window_size)
-    df_norm_train.drop(columns=df.columns[0], axis=1, inplace=True) #remove timestamp
-    #same on test
-    df_norm_test = moving_zscore(df_test, window_size)
-    df_norm_test.drop(columns=df.columns[0], axis=1, inplace=True)
-    #train segmentatiom
-    segments_train = segmentation(df_norm_train, window_seg)
-    array_list = [df.values for df in segments_train]
-    input_data = np.stack(array_list, axis=2)
-    input_data = np.transpose(input_data, (2, 0, 1))
-    print("Shape of input data array:", input_data.shape)
-    #test segmentation
-    segments_test = segmentation(df_norm_test, window_seg)
-    array_list_te = [df.values for df in segments_test]
-    test_data = np.stack(array_list_te, axis=2)
-    test_data = np.transpose(test_data, (2, 0, 1))
-    print("Shape of input data array:", test_data.shape)
-    #nn
-    input_data = np.asarray(input_data).astype(np.float32)
-    test_data = np.asarray(test_data).astype(np.float32)
-    reconstructed_data = oneD_CNN(input_data,test_data)
-    # Compute cumulative DMA error
-    sensor_indices_one = [0, 1, 2]  # n1, n4, n31 as sensor indices for zone 1
-    cdma_error = compute_cumulative_dma_error(reconstructed_data, sensor_indices_one,test_data)
-    print("Cumulative DMA Error shape:", cdma_error.shape)
+x_test = segmentation(df_test_value, window_seg)
+x_test = np.array(x_test)
+# Get test MAE loss.
+x_test_pred = model.predict(x_test)
+test_mae_loss = np.mean(np.abs(x_test_pred - x_test), axis=1)
+#test_mae_loss=test_mae_loss[:, :3]
+test_mae_loss = test_mae_loss.reshape((-1))
+
+plt.hist(test_mae_loss, bins=50)
+plt.xlabel("test MAE loss")
+plt.ylabel("No of samples")
+plt.show()
+
+anomalies = test_mae_loss > threshold
+print("Number of anomaly samples: ", np.sum(anomalies))
+print("Indices of anomaly samples: ", np.where(anomalies))
+
+anomalous_data_indices = []
+for data_idx in range(window_seg - 1, len(df_test_value) - window_seg + 1):
+    if np.all(anomalies[data_idx - window_seg + 1 : data_idx]):
+        anomalous_data_indices.append(data_idx)
+
+df_subset = df_test.iloc[anomalous_data_indices]
+fig, ax = plt.subplots()
+df_test.plot(legend=False, ax=ax)
+df_subset.plot(legend=False, ax=ax, color="r")
+plt.show()
